0c097dbd0981267d937773e611c9740a62898168
ordon <ordon1313@gmail.com>
2016-01-18 23:03:20 +0100

    set list to full

--- src/Main.hs
+++ src/Main.hs
@@ -15,10 +15,13 @@ solveKrotke (board) ((row,column),0) = sloveZero board row column
 solveKrotke (board) ((row,column),1) = board
 solveKrotke (board) ((row,column),2) = board
 solveKrotke (board) ((row,column),3) = board
-solveKrotke (board) ((row,column),4) = board
+solveKrotke (board) ((row,column),4) = sloveFour board row column
 
 sloveZero :: [[Integer]] -> Integer -> Integer -> [[Integer]]
-sloveZero (board) (row) (column) = setListToEmpty board [((row-1), (column-1))]
+sloveZero (board) (row) (column) = setListToEmpty board [((row-1), (column-1)),((row), (column-1)),((row-1), (column)),((row), (column))]
+
+sloveFour :: [[Integer]] -> Integer -> Integer -> [[Integer]]
+sloveFour (board) (row) (column) = setListToFull board [((row-1), (column-1)),((row), (column-1)),((row-1), (column)),((row), (column))]
 
 setListToEmpty :: [[Integer]] -> [(Integer, Integer)] -> [[Integer]]
 setListToEmpty (board) [] = board
@@ -33,3 +36,17 @@ setToEmptyInRow :: [Integer] -> [Integer] -> Integer -> [Integer]
 setToEmptyInRow [] (accumulator) (column) = accumulator
 setToEmptyInRow (value:row) (accumulator) (0) = setToEmptyInRow row (accumulator ++ [0]) (-1)
 setToEmptyInRow (value:row) (accumulator) (column) = setToEmptyInRow row (accumulator ++ [value]) (column-1)
+
+setListToFull :: [[Integer]] -> [(Integer, Integer)] -> [[Integer]]
+setListToFull (board) [] = board
+setListToFull (board) ((row, column):more) = setListToFull (setToFull board [] row column) more
+
+setToFull :: [[Integer]] -> [[Integer]] -> Integer -> Integer -> [[Integer]]
+setToFull [] (accumulator) (rowIndex) (column) = accumulator
+setToFull (row:board) (accumulator) (0) (column) = setToFull board (accumulator ++ [(setToFullInRow row [] column)]) (-1) column
+setToFull (row:board) (accumulator) (rowIndex) (column) = setToFull board (accumulator ++ [row]) (rowIndex-1) column
+
+setToFullInRow :: [Integer] -> [Integer] -> Integer -> [Integer]
+setToFullInRow [] (accumulator) (column) = accumulator
+setToFullInRow (value:row) (accumulator) (0) = setToFullInRow row (accumulator ++ [1]) (-1)
+setToFullInRow (value:row) (accumulator) (column) = setToFullInRow row (accumulator ++ [value]) (column-1)
